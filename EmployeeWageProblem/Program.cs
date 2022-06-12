41e461bbd1567a5be779d30fc6b56b216ce9810e
Siddhant <santoshshrivastava39@gmail.com>
2022-06-12 10:25:21 +0530

    Added the UC1 solution

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -6,7 +6,25 @@ namespace EmployeeWageProblem
     {
         static void Main(string[] args)
         {
+            Program.CheckEmployeeAttendance();
+        }
+
+        public static void CheckEmployeeAttendance()
+        {
+
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Random random = new Random();
+            int number = random.Next(0, 2);
+            if (number == 0)
+            {
+                Console.WriteLine("Employee is absent");
+                
+            }
+            else
+            {
+                Console.WriteLine("Employee is present");
+                
+            }
         }
     }
 }
