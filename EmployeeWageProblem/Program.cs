a43d5074e43e8b6ce63bc1e23d1ee2b3f2be0bee
Siddhant <santoshshrivastava39@gmail.com>
2022-06-12 10:40:17 +0530

    Added the UC2 solution

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -13,17 +13,24 @@ namespace EmployeeWageProblem
         {
 
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+            int FullDayHr = 8;
+            int WagePerHr = 20;
+            int totalWage = 0;
+
             Random random = new Random();
             int number = random.Next(0, 2);
             if (number == 0)
             {
                 Console.WriteLine("Employee is absent");
-                
+                Console.WriteLine("Employee's total wage  is "+ totalWage);
+
             }
             else
             {
                 Console.WriteLine("Employee is present");
-                
+                totalWage = FullDayHr * WagePerHr;
+                Console.WriteLine("Employee's total wage  is " + totalWage);
             }
         }
     }
