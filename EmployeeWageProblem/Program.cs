12b8964822a05254dab153d8bb5b23f2805db634
Siddhant <santoshshrivastava39@gmail.com>
2022-06-12 10:50:49 +0530

    Added the UC3 solution

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -17,21 +17,28 @@ namespace EmployeeWageProblem
             int FullDayHr = 8;
             int WagePerHr = 20;
             int totalWage = 0;
-
+            int PartTimeHr = 4;
             Random random = new Random();
-            int number = random.Next(0, 2);
+            int number = random.Next(0, 3);
             if (number == 0)
             {
                 Console.WriteLine("Employee is absent");
-                Console.WriteLine("Employee's total wage  is "+ totalWage);
+                
 
             }
-            else
+            else if(number==1)
             {
                 Console.WriteLine("Employee is present");
                 totalWage = FullDayHr * WagePerHr;
-                Console.WriteLine("Employee's total wage  is " + totalWage);
+              
             }
+            else
+            {
+                Console.WriteLine("Employee is present and part time employee");
+                totalWage = PartTimeHr * WagePerHr;
+               
+            }
+            Console.WriteLine("Employee's total wage  is " + totalWage);
         }
     }
 }
